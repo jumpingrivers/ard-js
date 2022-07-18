d3513db9272fbdee0515878b0b98f88c21127c44
Tim Brock <info@datatodisplay.com>
2022-07-18 17:18:55 +0100

    Split Sankey into submodules

---
+++ src/sankey/validate.mjs
@@ -0,0 +1,36 @@
+const validateData = function(data) {
+  if (!Array.isArray(data)) {
+    throw new Error('data must be an array');
+  }
+};
+
+
+const validateSteps = function(steps) {
+  if (!Array.isArray(steps)) {
+    throw new Error('steps must be an array');
+  }
+
+  if (steps.length < 2) {
+    throw new Error('steps array must have at least two elements');
+  }
+
+  const stepTypesGood = steps.every(function(step) {
+    if (typeof step === 'string') { return true; }
+    if (Array.isArray(step) && step.every(s => typeof s === 'string')) {
+      return true;
+    }
+    return false;
+  });
+
+  if (!stepTypesGood) {
+    throw new Error('steps elements must be strings or arrays of strings');
+  }
+
+  const flatSteps = steps.flat();
+  if ((new Set(flatSteps).size < flatSteps.length)) {
+    throw new Error('steps cannot contain a repeated value');
+  }
+};
+
+
+export { validateData, validateSteps };

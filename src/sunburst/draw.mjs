21ac08cc510bf55da26b78a190c1c8b66b25fc94
Tim Brock <info@datatodisplay.com>
2022-08-01 17:20:04 +0100

    Add basic hover behaviour to sunburst

--- src/sunburst/draw.mjs
+++ src/sunburst/draw.mjs
@@ -14,6 +14,7 @@ const drawSunburst = function(sunburstData) {
   const width = baseWidth;
   const height = baseWidth;
   const radius = width / 2;
+  const breadcrumbHeight = 50;
   
   const shadow = select(container.node().shadowRoot);
 
@@ -23,8 +24,18 @@ const drawSunburst = function(sunburstData) {
     .attr('viewBox', `${-radius} ${-radius} ${width} ${height}`)
     .datum(sunburstData);
 
-  const arcLayer = svg.append('g')
-    .attr('id', 'arc-layer');
+  const breadcrumb = shadow.select('#breadcrumb')
+    .attr('viewBox', `0 0 ${width} ${breadcrumbHeight}`);
+
+  const baseLayer = svg.append('g')
+    .attr('id', 'base-layer');
+
+  const textLayer = svg.append('g')
+    .attr('id', 'text-layer');
+
+  const hoverLayer = svg.append('g')
+    .attr('id', 'hover-layer')
+    .style('pointer-events', 'none');
 
   const pathGenerator = arc()
     .startAngle(d => d.x0)
@@ -35,7 +46,44 @@ const drawSunburst = function(sunburstData) {
   const part = partition()
     .size([2 * Math.PI, Math.pow(radius, 2)])(sunburstData);
 
-  arcLayer.selectAll('path')
+  const mouseover = function(_, d) {
+    const data = d.ancestors().filter(d => d.depth);
+
+    hoverLayer.selectAll('path')
+      .data(data)
+      .enter()
+      .append('path')
+      .attr('d', pathGenerator)
+      .style('fill', 'red')
+      .style('stroke', 'black');
+
+    const percent = (d.value / sunburstData.value) * 100;
+
+    textLayer
+      .append('text')
+      .text(`${percent.toPrecision(2)}%`)
+      .style('text-anchor', 'middle')
+      .style('font-size', '75px')
+      .style('dominant-baseline', 'middle');
+
+    breadcrumb.selectAll('span')
+      .data(data.slice().reverse())
+      .enter()
+      .append('span')
+      .text(function(d) {
+        const prefix = '';
+        const suffix = '';
+        return `${prefix}${d.data.name}${suffix}`;
+      });
+  };
+
+  const mouseout = function() {
+    hoverLayer.text('');
+    textLayer.text('');
+    breadcrumb.text('');
+  };
+ 
+  baseLayer.selectAll('path')
     .data(part.descendants().filter(function(d) {
       return d.depth;
     }))
@@ -43,7 +91,9 @@ const drawSunburst = function(sunburstData) {
     .append('path')
     .attr('d', pathGenerator)
     .style('fill', '#add8e6')
-    .style('stroke', '#888888');
+    .style('stroke', '#888888')
+    .on('mouseover', mouseover)
+    .on('mouseout', mouseout);
 };
 
 

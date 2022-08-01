2a2146f92f91d829253d521895fd7fcc0c62d7e2
Tim Brock <info@datatodisplay.com>
2022-08-01 15:08:30 +0100

    Render basic sunburst

--- src/sunburst/draw.mjs
+++ src/sunburst/draw.mjs
@@ -1,10 +1,10 @@
 import { select } from 'd3-selection';
 import 'd3-transition';
+import { partition } from 'd3-hierarchy';
+import { arc } from 'd3-shape';
 import vizTemplate from './viz-templates/index.html';
 
 const baseWidth = 1000;
-const padding = 10;
-const topPadding = 5 * padding;
 
 
 const drawSunburst = function(sunburstData) {
@@ -13,16 +13,37 @@ const drawSunburst = function(sunburstData) {
   
   const width = baseWidth;
   const height = baseWidth;
-  const widthWithPadding = width + 2 * padding;
-  const heightWithPadding = height + padding + topPadding;
+  const radius = width / 2;
   
   const shadow = select(container.node().shadowRoot);
 
   shadow.html(vizTemplate);
 
-  shadow.select('#graphic')
-    .attr('viewBox', `0 0 ${widthWithPadding} ${heightWithPadding}`)
+  const svg = shadow.select('#graphic')
+    .attr('viewBox', `${-radius} ${-radius} ${width} ${height}`)
     .datum(sunburstData);
+
+  const arcLayer = svg.append('g')
+    .attr('id', 'arc-layer');
+
+  const pathGenerator = arc()
+    .startAngle(d => d.x0)
+    .endAngle(d => d.x1)
+    .innerRadius(d => Math.sqrt(d.y0))
+    .outerRadius(d => Math.sqrt(d.y1));
+
+  const part = partition()
+    .size([2 * Math.PI, Math.pow(radius, 2)])(sunburstData);
+
+  arcLayer.selectAll('path')
+    .data(part.descendants().filter(function(d) {
+      return d.depth;
+    }))
+    .enter()
+    .append('path')
+    .attr('d', pathGenerator)
+    .style('fill', '#add8e6')
+    .style('stroke', '#888888');
 };
 
 

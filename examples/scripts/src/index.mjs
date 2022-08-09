fe7a5c9745d676edc024126cfa15ae7bc01f2a85
Tim Brock <info@datatodisplay.com>
2022-08-09 12:46:00 +0100

    Color-overrides example

--- examples/scripts/src/index.mjs
+++ examples/scripts/src/index.mjs
@@ -1,8 +1,15 @@
 /* globals utviz */
 import { data, steps } from '../../data/datasets.mjs';
 
+const colorOverrides = [
+  { group: 'student_type', color: 'teal' },
+  { name: 'true', color: 'pink'},
+  { group: 'student_type', name: 'transfer', color: '#446688' }
+];
+
 const sankey = utviz
   .createSankey(data, steps)
+  .colorOverrides(colorOverrides)
   .render();
 
 const sankeyContainer = document.querySelector('#sankey-container');
@@ -10,6 +17,7 @@ sankeyContainer.appendChild(sankey.viz);
 
 const sunburst = utviz
   .createSunburst(data, steps.flat())
+  .colorOverrides(colorOverrides)
   .render();
 
 const sunburstContainer = document.querySelector('#sunburst-container');
